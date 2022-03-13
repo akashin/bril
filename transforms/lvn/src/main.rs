5a9b1618d0fe9cba496e2993cab31a12ed66ac50
Andrei Kashin <kashin.andrej@gmail.com>
2022-03-13 16:24:58 +0000

    [lvn] Support more instructions from bril IR

--- transforms/lvn/src/main.rs
+++ transforms/lvn/src/main.rs
@@ -19,16 +19,18 @@ struct Function {
     args: Vec<String>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
 #[serde(deny_unknown_fields)]
 struct Instruction {
-    op: String,
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    op: Option<String>,
 
     #[serde(skip_serializing_if = "Option::is_none")]
     dest: Option<String>,
 
     #[serde(skip_serializing_if = "Option::is_none")]
-    value: Option<i64>,
+    value: Option<serde_json::Value>,
 
     #[serde(rename = "type")]
     #[serde(skip_serializing_if = "Option::is_none")]
@@ -37,6 +39,75 @@ struct Instruction {
     #[serde(default)]
     #[serde(skip_serializing_if = "Vec::is_empty")]
     args: Vec<String>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Vec::is_empty")]
+    labels: Vec<String>,
+
+    #[serde(default)]
+    #[serde(skip_serializing_if = "Option::is_none")]
+    label: Option<String>,
+}
+
+impl Instruction {
+    fn is_terminator(&self) -> bool {
+        match &self.op {
+            Some(op) => {
+                op == "jmp" || op == "cond"
+            }
+            None => false,
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Block {
+    instrs: Vec<Instruction>,
+    next_blocks: Vec<i64>,
+}
+
+#[derive(Debug)]
+struct ControlFlowGraph {
+    blocks: Vec<Block>,
+}
+
+impl ControlFlowGraph {
+    fn to_instrs(&self) -> Vec<Instruction> {
+        let mut result = Vec::<Instruction>::new();
+        for block in &self.blocks {
+            result.extend(block.instrs.clone());
+        }
+        result
+    }
+}
+
+fn construct_control_flow_graph(function: &Function) -> ControlFlowGraph {
+    let mut cfg = ControlFlowGraph { blocks: Vec::new() };
+
+    let mut cur_block = Block {
+        instrs: Vec::new(),
+        next_blocks: Vec::new(),
+    };
+
+    for instr in &function.instrs {
+        cur_block.instrs.push(instr.clone());
+        if instr.is_terminator() {
+            cfg.blocks.push(cur_block);
+            cur_block = Block {
+                instrs: Vec::new(),
+                next_blocks: Vec::new(),
+            };
+        }
+    }
+    if !cur_block.instrs.is_empty() {
+        cfg.blocks.push(cur_block);
+    }
+
+    cfg
+}
+
+fn eliminate_dead_code(cfg: ControlFlowGraph) -> ControlFlowGraph {
+    cfg
 }
 
 fn main() {
@@ -46,7 +117,13 @@ fn main() {
         .read_to_string(&mut buffer)
         .expect("Failed to read input");
 
-    let program: Program = serde_json::from_str(&buffer).expect("Failed to parse program IR");
+    let mut program: Program = serde_json::from_str(&buffer).expect("Failed to parse program IR");
+    for function in &mut program.functions {
+        let cfg = construct_control_flow_graph(function);
+        let cfg = eliminate_dead_code(cfg);
+        dbg!(&cfg);
+        function.instrs = cfg.to_instrs();
+    }
 
     println!(
         "{}",
